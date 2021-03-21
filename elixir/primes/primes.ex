06a6ff1bff41b10614eba9a5f5b407b04da11663
angrajales <agrajal7@eafit.edu.co>
2021-03-21 12:39:24 -0500

    Further optimization for exercise 3

--- elixir/primes/primes.ex
+++ elixir/primes/primes.ex
@@ -2,12 +2,12 @@
 n = 15578
 Enum.filter(
   1..n,
-  &(&1 == 2 || Enum.all?(2..&1 - 1, fn x -> rem(&1, x) != 0 end))
+  &(&1 > 1 and (&1 == 2 || &1 == 3 || Enum.all?(2..trunc(:math.sqrt(&1)), fn x -> rem(&1, x) != 0 end)))
 ) |> Enum.each(&(IO.puts/1))
 
 IO.puts("Number of primes less than #{n} is ")
 # Number of primes
 Enum.filter(
   1..n,
-  &(&1 == 2 || Enum.all?(2..&1 - 1, fn x -> rem(&1, x) != 0 end))
+  &(&1 > 1 and (&1 == 2 || &1 == 3 || Enum.all?(2..trunc(:math.sqrt(&1)), fn x -> rem(&1, x) != 0 end)))
 ) |> Enum.count |> IO.puts()
